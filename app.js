32a763315e8724a308d0b26b6b121414d0947dc4
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2022-04-08 11:29:44 -0500

    add modal

---
+++ app.js
@@ -0,0 +1,9 @@
+const formTravel = document.getElementById("form-travel").value;
+const searchTravel = document.getElementById("search-travel").value;
+const timeTravel = document.getElementById("travel-time").value;
+const dateTravel = document.getElementById("date-travel").value;
+
+console.log(formTravel);
+console.log(searchTravel);
+console.log(timeTravel);
+console.log(dateTravel);
