e6f4ac9b58d5ee4fe1d619ce71df8437967f8f47
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2022-05-01 12:30:34 -0500

    first modal panel added

--- js/panel.js
+++ js/panel.js
@@ -1,10 +1,77 @@
 const buttons = document.querySelectorAll("#panel-button button");
-const modal = document.getElementById("modal");
+const modalEl = {
+	modal: document.getElementById("modal"),
+	title: document.querySelector("#modal h3"),
+	label: document.querySelectorAll("#modal label"),
+	input: document.querySelectorAll("#modal input"),
+	alert: document.querySelector("#modal h5"),
+};
+
+let config = {
+	create: {
+		title: "Crear viaje",
+		label: {
+			first: "Ruta",
+			second: "Bus",
+			third: "Chofer",
+		},
+		input: {
+			first: "Ingrese la ruta",
+			second: "Ingrese el bus",
+			third: "Ingrese el chofer",
+		},
+		submit: "Crear",
+		alert: "Viaje creado éxitosamente",
+	},
+};
+
+const listColor = {
+	sucessful: "#029918",
+	unsuccessful: "#ff3333",
+	enable: "#ff477e",
+	disable: "#ff8bac",
+};
 
 const setButton = (element) => {
 	switch (element.target.id) {
 		case "create": {
-			console.log("it works");
+			modalEl.title.innerHTML = config.create.title;
+			modalEl.label.forEach((el) => {
+				switch (el.id) {
+					case "label-first": {
+						el.innerHTML = config.create.label.first;
+						break;
+					}
+					case "label-second": {
+						el.innerHTML = config.create.label.second;
+						break;
+					}
+					case "label-third": {
+						el.innerHTML = config.create.label.third;
+						break;
+					}
+				}
+			});
+			modalEl.input.forEach((el) => {
+				switch (el.id) {
+					case "in-first": {
+						el.placeholder = config.create.input.first;
+						break;
+					}
+					case "in-second": {
+						el.placeholder = config.create.input.second;
+						break;
+					}
+					case "in-third": {
+						el.placeholder = config.create.input.third;
+						break;
+					}
+					case "submit": {
+						el.value = config.create.submit;
+						break;
+					}
+				}
+			});
 			break;
 		}
 	}
@@ -12,12 +79,18 @@ const setButton = (element) => {
 
 buttons.forEach((element) => {
 	element.addEventListener("click", (e) => {
-		e.preventDefault();
 		modal.style.visibility = "visible";
 		setButton(e);
 	});
 });
 
-// modal.addEventListener("click", (e) => {
-// 	e.preventDefault();
-// });
+modalEl.modal.addEventListener("submit", (e) => {
+	e.preventDefault();
+	modalEl.alert.style.color = listColor.sucessful;
+	modalEl.alert.innerHTML = config.create.alert;
+	modalEl.alert.style.visibility = "visible";
+	setTimeout(() => {
+		modalEl.modal.style.visibility = "hidden";
+		modalEl.alert.style.visibility = "hidden";
+	}, 3000);
+});
